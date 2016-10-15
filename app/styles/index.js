fe82beff0c6c70fba9ec0ab143ec6932a23bf878
dwicao <lutfiandwica@gmail.com>
2016-10-15 22:28:04 +0700

    first commit

---
+++ app/styles/index.js
@@ -0,0 +1,25 @@
+var styles = {
+	body: {
+		backgroundColor: '#68a3a5'
+	},
+	wrapper: {
+		backgroundColor: '#538284',
+		color: '#22c5e2',
+		width: 350,
+		height: 449,
+		textAlign: 'center',
+		margin: '40px auto',
+		paddingTop: 50,
+	},
+	title: {
+		fontSize: 28,
+		marginBottom: 50,
+	},
+	timer: {
+		fontSize: 56,
+		textAlign: 'center',
+		marginBottom: 50,
+	}
+};
+
+module.exports = styles;
