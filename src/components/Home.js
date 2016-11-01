9da077f887c4f4c6dc1ca24473740b45c595e0bf
dwicao <lutfiandwica@gmail.com>
2016-11-01 12:42:39 +0700

    add bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import styles from './Home.css';
+import {styles} from './Home_style';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Home extends Component {
 	render() {
 		return (
-			<div className={styles.home}>
-				<h1>Hello World</h1>
+			<div className="container">
+				<div className="row">
+					<h1 style={styles.title}>
+						Hello World
+					</h1>
+				</div>
 			</div>
 		);
 	}
