d7123ac1e30e21e30306519022f4002b70ae1eb3
dwicao <lutfiandwica@gmail.com>
2017-04-14 15:32:05 +0700

    fix env var

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,10 @@ class Home extends Component {
 
   }
 
+  componentDidMount() {
+    console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+  }
+
 
   render() {
     return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var prod = process.argv.indexOf('-p') !== -1;
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: __dirname + '/src/index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
 
-module.exports = {
+var config = {
 	entry: [
 		'./src/index.js'
 	],
@@ -29,13 +30,26 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new webpack.DefinePlugin({
-	   		'process.env': {
-	     		 NODE_ENV: JSON.stringify('production')
-	    	}
-	  	}),
-    new webpack.NoErrorsPlugin(), 
+    	new webpack.NoErrorsPlugin(), 
 		new ExtractTextPlugin('styles.css'),
 		HtmlWebpackPluginConfig,
 	]
 };
+
+config.plugins = config.plugins || [];
+
+if (prod) {
+  config.plugins.push(new webpack.DefinePlugin({
+      'process.env': {
+          'NODE_ENV': `"production"`
+      }
+  }));
+} else {
+  config.plugins.push(new webpack.DefinePlugin({
+      'process.env': {
+          'NODE_ENV': `"dev"`
+      }
+  }));
+}
+
+module.exports = config;
